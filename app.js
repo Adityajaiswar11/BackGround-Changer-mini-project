530fab6dbcfd0b5cfeb4de5efa738865a173a38f
Aditya Jaiswar <adityajaiswar11@gmail.com>
2024-01-24 20:38:20 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const parent = document.getElementById("parent");
+const child = document.getElementById("child");
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const para = document.getElementById("para");
+const colorName = document.getElementById("colorName");
+
+input.addEventListener("change", ColorChange);
+
+function ColorChange(e) {
+  const inputValue = e.target.value+" ";
+
+  const color = inputValue.toLowerCase();
+  child.style.backgroundColor = color;
+  colorName.innerHTML = color.toUpperCase();
+  colorName.style.color = color;
+  para.innerHTML="Great! You have changed  the color. Try again.";
+}
+
+btn.addEventListener("click", () => {
+
+  ColorChange();
+});
